eb70dfc5f7ab31c144a83100adf9fc8b9172b35c
Josué Batista <josuebatista.ti@gmail.com>
2018-10-29 11:38:01 -0300

    first

---
+++ ParOuImpar.py
@@ -0,0 +1,10 @@
+num = int(input('Digite um valor: '))
+def par(x):
+    return (x % 2 == 0)
+def par_impar(x):
+    if par(x):
+        return 'Par'
+    else:
+        return 'Ímpar'
+print(par_impar(num))
+
