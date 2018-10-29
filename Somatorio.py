e0ffb9cf658fc0d4301b66835dc7f6d43421b329
Josué Batista <josuebatista.ti@gmail.com>
2018-10-29 15:11:42 -0300

    up

--- Somatorio.py
+++ Somatorio.py
@@ -1,7 +1,16 @@
-def somatorio():
-    a = 0
-    n = int(input('Digite o numero: '))
-    for i in range(n + 1):
-        a += i
-    return a
-print('Soma dos numeros =',somatorio())
+# def somatorio():
+#     a = 0
+#     n = int(input('Digite o numero: '))
+#     for i in range(n + 1):
+#         a += i
+#     return a
+# print('Soma dos numeros =',somatorio())
+
+def main():
+    num = int(input("Digite um inteiro [999 para terminar]: "))
+    soma = 0
+    while num != 999:
+        soma = soma + num
+        num = int(input("Digite um inteiro [999 para terminar]: "))
+    print("A soma foi: ", soma)
+main()
